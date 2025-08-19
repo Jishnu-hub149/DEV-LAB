567906ffa53cb4e7dade44e4c00d76688b96d125
Jishnu-hub149 <jishnucharen.v.b.2023.cse@ritchennai.edu.in>
2025-08-19 09:57:05 +0530

    Create DEV EXP 1.py

---
+++ DEV EXP 1.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+x = [1, 2, 3, 4]
+y = [10, 20, 25, 30]
+plt.plot(x, y)
+plt.title(&quot;Simple Line Chart&quot;)
+plt.xlabel(&quot;X-axis&quot;)
+plt.ylabel(&quot;Y-axis&quot;)
+plt.show()
