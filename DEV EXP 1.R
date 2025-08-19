ac0734f1c1cb02658b5cef9a9ba0629bd00d6bda
Jishnu-hub149 <jishnucharen.v.b.2023.cse@ritchennai.edu.in>
2025-08-19 09:58:07 +0530

    Create DEV EXP 1.R

---
+++ DEV EXP 1.R
@@ -0,0 +1,3 @@
+x &lt;- c(1, 2, 3, 4)
+y &lt;- c(10, 20, 25, 30)
+plot(x, y, type=&quot;o&quot;, col=&quot;blue&quot;, main=&quot;Simple Line Plot&quot;, xlab=&quot;X-axis&quot;, ylab=&quot;Y-axis&quot;)
